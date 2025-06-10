3b032635be00d096ca060101ccb923a3e4e70b2d
Akshat Kala <itzakshat706@gmail.com>
2025-06-10 10:59:04 +0530

    made responsive commit

--- src/components/main/intro.jsx
+++ src/components/main/intro.jsx
@@ -2,22 +2,52 @@ import "./intro.css";
 import profileLogo from "../../assets/profile.png";
 
 export default function Intro() {
-    return(
-        <div className="intro-section body-font" id="home">
-            <div className="left-pannel">
-                <p className="small-text">HELLO!</p>
-                <h2>I'm <span>Akshat Kala</span></h2>
-                <p className="description">a freelancing web developer helping out people round the globe</p>
-                <div className="cta-buttons">
-                    <button>Hire Me</button>
-                    <button><a href="#projects">My Works</a></button>
-                </div>
-            </div>
+    function screenWidth(){
+        if (window.innerWidth <= 833) {
+            return (
+                <div className="intro-section body-font" id="home">
+                    <div className="right-pannel">
+                        <div className="bgoval"></div>
+                        <img src={profileLogo} alt="Akshat Kala" className="profile-pic"/>
+                    </div>
 
-            <div className="right-pannel">
-                <div className="bgoval"></div>
-                <img src={profileLogo} alt="Akshat Kala" className="profile-pic"/>
-            </div>
-        </div>
+                    <div className="left-pannel">
+                        <p className="small-text">HELLO!</p>
+                        <h2>I'm <span>Akshat Kala</span></h2>
+                        <p className="description">a freelancing web developer helping out people round the globe</p>
+                        <div className="cta-buttons">
+                            <button>Hire Me</button>
+                            <button><a href="#projects">My Works</a></button>
+                        </div>
+                    </div>
+
+
+                </div>
+            )
+
+        }
+        else{
+            return (
+                <div className="intro-section body-font" id="home">
+                    <div className="left-pannel">
+                        <p className="small-text">HELLO!</p>
+                        <h2>I'm <span>Akshat Kala</span></h2>
+                        <p className="description">a freelancing web developer helping out people round the globe</p>
+                        <div className="cta-buttons">
+                            <button>Hire Me</button>
+                            <button><a href="#projects">My Works</a></button>
+                        </div>
+                    </div>
+
+                    <div className="right-pannel">
+                        <div className="bgoval"></div>
+                        <img src={profileLogo} alt="Akshat Kala" className="profile-pic"/>
+                    </div>
+                </div>
+            )
+        }
+    }
+    return(
+        screenWidth()
     )
 }
