5e9e73c48c0ae2cb523eb57fe4c2776a15c2417d
Akshat Kala <itzakshat706@gmail.com>
2025-06-11 23:20:46 +0530

    made responsive commit

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -1,7 +1,28 @@
 import "./navbar.css";
 
 export default function Navbar() {
-    return (
+    function callNav() {
+        const slideNav = document.querySelector(".slideNavHidden");
+        slideNav.classList.add("slideNav");
+
+        slideNav.classList.remove("slideNavHidden");
+    }
+
+    function hideNav() {
+        const slideNav = document.querySelector(".slideNav");
+        slideNav.classList.add("slideNavHidden");
+
+        slideNav.classList.remove("slideNav");
+    }
+
+    function Exit(){
+        hideNav()
+    }
+
+
+    if (window.innerWidth >= 833){
+           return (
+        
         <>
             <div className="nav nav-font">
                 <div className="left"><a href="#">AKSHAT</a></div>
@@ -19,4 +40,32 @@ export default function Navbar() {
         </>
 
     )
+    }
+    else{
+    return (
+        
+        <>
+            <div className="nav nav-font">
+                <div className="left"><a href="#">AKSHAT</a></div>
+                <div className="right">
+                    <i class="fa-solid fa-ellipsis-vertical threedot" onClick={callNav}></i>
+                    <div className="slideNavHidden">
+                        <i class="fa-solid fa-xmark cross" onClick={hideNav}></i>
+                        <ul className="unorderList">
+                            <li><a href="#home" className="active" onClick={Exit}>Home</a></li>
+                            <li><a href="#about" onClick={Exit}>About</a></li>
+                            <li><a href="#resume" onClick={Exit}>Resume</a></li>
+                            <li><a href="#skills" onClick={Exit}>Skills</a></li>
+                            <li><a href="#projects" onClick={Exit}>Projects</a></li>
+                            <li><a href="#contact" onClick={Exit}>Contact</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </>
+
+    )
+    }
+
 }
